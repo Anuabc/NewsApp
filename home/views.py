ed94a09175c8c65a9e8d5efe1a83d26dbc8bf682
Mayank Gupta <67818466+Mayank-Creater@users.noreply.github.com>
2024-08-10 20:27:50 +0000

    Fixed the sizing of each card and made the buttons functional with title also.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    url = "https://timesofindia.indiatimes.com/"
+    page_request = requests.get(url)
+    data = page_request.content
+    soup = BeautifulSoup(data,"html.parser")
+
+    card = {}
+
+    counter = 0
+    for divtag in soup.find_all('div', {'class': 'col_l_6'}):
+        for atag in divtag.find_all('a', {'class': 'Hn2z7'}):
+            imgtag = atag.find('img')
+            card[counter]  = {'title':imgtag['alt'], 'link':atag['href']}
+            counter += 1
+
+    length = len(card)
+    card['length'] = [x for x in range(length)]
+
+    context = {
+        'card': card,
+    }
+
+    return render(request, 'index.html', context=context)
