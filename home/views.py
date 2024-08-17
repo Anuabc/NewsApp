ce3714daef01c44839d638ab3468a8d72080b338
Mayank Gupta <67818466+Mayank-Creater@users.noreply.github.com>
2024-08-17 18:17:48 +0000

    Fixed image loading

--- home/views.py
+++ home/views.py
@@ -6,14 +6,15 @@ from bs4 import BeautifulSoup
 def home(request):
     
     header ={
-        "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "no-cache",
-        "referer": "https://www.hindustantimes.com/india-news/page-1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        "Sec-GPC": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Brave\";v=\"127\", \"Chromium\";v=\"127\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "Windows"
         }
 
-    url = "https://www.hindustantimes.com/india-news/page-1"
+    url = "https://www.hindustantimes.com/"
     page_request = requests.get(url, headers=header)
     data = page_request.content
     soup = BeautifulSoup(data,"html.parser")
@@ -23,18 +24,24 @@ def home(request):
     counter = 0
 
     for divtag in soup.find_all('div', {'class': 'articleClick'}):
-        link = divtag['data-weburl']
-        imgtag = divtag.find('img')
-        img = imgtag['src']
-        title = imgtag['alt']
-        card[counter]  = {'title':title, 'link':link, 'img':img}
-        counter += 1
+        if 'liveStory' not in divtag['class']:
+            link = divtag['data-weburl']
+            imgtag = divtag.find('img')
+            try:
+                img = imgtag['data-src']
+            except:
+                img = imgtag['src']
+            title = imgtag['alt']
+            title = ' '.join(title.split(' ')[:10])+'...'
+            card[counter]  = {'title':title, 'link':link, 'img':img}
+            counter += 1
 
-    length = len(card)
-    card['length'] = [x for x in range(length)]
+    # length = len(card)
+    # card['length'] = [x for x in range(length)]
 
     context = {
         'card': card,
     }
 
+    print(card)
     return render(request, 'index.html', context=context)
