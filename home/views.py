be622d662640b20024c6b3dd7a391c5471f3db21
Mayank Gupta <67818466+Mayank-Creater@users.noreply.github.com>
2024-08-17 17:54:28 +0000

    Fetches data from HindustanTimes

--- home/views.py
+++ home/views.py
@@ -4,19 +4,31 @@ from bs4 import BeautifulSoup
 
 # Create your views here.
 def home(request):
-    url = "https://timesofindia.indiatimes.com/"
-    page_request = requests.get(url)
+    
+    header ={
+        "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "referer": "https://www.hindustantimes.com/india-news/page-1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+        }
+
+    url = "https://www.hindustantimes.com/india-news/page-1"
+    page_request = requests.get(url, headers=header)
     data = page_request.content
     soup = BeautifulSoup(data,"html.parser")
 
     card = {}
 
     counter = 0
-    for divtag in soup.find_all('div', {'class': 'col_l_6'}):
-        for atag in divtag.find_all('a', {'class': 'Hn2z7'}):
-            imgtag = atag.find('img')
-            card[counter]  = {'title':imgtag['alt'], 'link':atag['href']}
-            counter += 1
+
+    for divtag in soup.find_all('div', {'class': 'articleClick'}):
+        link = divtag['data-weburl']
+        imgtag = divtag.find('img')
+        img = imgtag['src']
+        title = imgtag['alt']
+        card[counter]  = {'title':title, 'link':link, 'img':img}
+        counter += 1
 
     length = len(card)
     card['length'] = [x for x in range(length)]
